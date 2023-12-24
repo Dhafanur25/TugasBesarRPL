567a4409f36d451baa5ba097ce0b4be5ec96ad50
Dhafa Nur Fadhilah <121477165+Dhafanur25@users.noreply.github.com>
2023-12-24 21:30:12 +0700

    Delete src directory

--- src/main/BD.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public interface BD {
-    public int getIDAccKon();
-    public int getIDAccUser();
-    public void setIDAccKon(int IDAccKon);
-    public void setIDAccUser(int IDAccUser);
-    
-}
